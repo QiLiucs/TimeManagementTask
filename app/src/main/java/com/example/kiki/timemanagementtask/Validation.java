3ca2cb5585c7e31fdc2d85007467d4aad006cb18
qi liu <liuqi@usc.edu>
2019-12-31 13:55:59 +0800

    compare date

--- app/src/main/java/com/example/kiki/timemanagementtask/Validation.java
+++ app/src/main/java/com/example/kiki/timemanagementtask/Validation.java
@@ -2,6 +2,10 @@ package com.example.kiki.timemanagementtask;
 
 import android.widget.EditText;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Validation {
     static boolean checkEmpty(EditText editText){
         String s = editText.getText().toString();
@@ -16,13 +20,20 @@ public class Validation {
     static boolean checkLaterThan(EditText startDateEdit, EditText ddlEdit){
         if(checkEmpty(startDateEdit) || checkEmpty(ddlEdit))
             return false;
-        if(startDateEdit.getText().toString().compareTo(ddlEdit.getText().toString()) > 0){
-            startDateEdit.setError("should be earlier than deadline");
-            startDateEdit.requestFocus();
-            return false;
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            Date start = sdf.parse(startDateEdit.getText().toString());
+            Date end = sdf.parse(ddlEdit.getText().toString());
+            if(start.after(end)){
+                startDateEdit.setError("should be earlier than deadline");
+                startDateEdit.requestFocus();
+                return false;
+            }
+            return true;
+        } catch (ParseException e) {
+            e.printStackTrace();
         }
         return true;
-
     }
 
     public static boolean isValid(EditText... editTexts){
